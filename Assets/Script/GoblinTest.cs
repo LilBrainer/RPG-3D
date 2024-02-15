a0256277d3f47e815dbec27f11fad4910fe94cde
LilBrainer <josepuy71@gmail.com>
2024-02-15 15:35:42 +0100

    Adding Project

---
+++ Assets/Script/GoblinTest.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoblinTest : MonoBehaviour
+{
+    public float velocidad = 0.0f;
+    public float aceleracion = 0.1f;
+    public float desaceleracion = 0.5f;
+    private Animator m_Animator;
+
+    private void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool mover = Input.GetKey(KeyCode.W);
+        bool aim = Input.GetKey(KeyCode.P);
+
+        if(mover && velocidad < 1.0f)
+        {
+            velocidad += Time.deltaTime * aceleracion;
+        }
+
+        if(!mover && velocidad > 0.0f) 
+        {
+            velocidad -= Time.deltaTime * desaceleracion;
+        }
+
+        if(!mover && velocidad < 0.0f) 
+        {
+            velocidad = 0.0f;
+        }
+
+        if (aim)
+        {
+            m_Animator.SetBool("Aiming", !m_Animator.GetBool("Aiming"));
+        }
+
+        
+
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            m_Animator.SetBool("Dancing", !m_Animator.GetBool("Dancing"));
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            m_Animator.SetBool("TPsoe", !m_Animator.GetBool("TPsoe"));
+        }
+               
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            m_Animator.SetBool("Falling", !m_Animator.GetBool("Falling"));
+        }
+
+        m_Animator.SetFloat("Velocidad", velocidad);
+    }
+}
